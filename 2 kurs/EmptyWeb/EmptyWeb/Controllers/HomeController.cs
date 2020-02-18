ee2306357fc80391bf3295078ead6b9011cc5f90
Rustem Salimov <rust1k@mail.ru>
2020-02-18 13:54:40 +0300

    [add] 2k 2s 1 lesson

---
+++ 2 kurs/EmptyWeb/EmptyWeb/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace EmptyWeb
+{
+	public class HomeController
+	{
+		public async Task GetForm(HttpContext context)
+		{
+			await context.Response.WriteAsync(File.ReadAllText("Views\\index.html"));
+		}
+
+		public async Task AddEntry(HttpContext context)
+		{
+			string filePath = "Files";
+
+			int fileCount = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories).Length;
+			fileCount++;
+
+			foreach (var formFile in context.Request.Form.Files)
+			{
+				if (formFile.Length > 0)
+				{
+					string newFile = Path.Combine(filePath, fileCount + System.IO.Path.GetExtension(formFile.FileName));
+					using (var inputStream = new FileStream(newFile, FileMode.Create))
+					{
+						// read file to stream
+						await formFile.CopyToAsync(inputStream);
+						// stream to byte array
+						byte[] array = new byte[inputStream.Length];
+						inputStream.Seek(0, SeekOrigin.Begin);
+						inputStream.Read(array, 0, array.Length);
+						// get file name
+						string fName = formFile.FileName;
+					}
+				}
+			}
+
+			string name = context.Request.Form["name"];
+			string text = context.Request.Form["text"];
+			string txtFileName = Path.Combine(filePath, fileCount + ".txt");
+			File.AppendAllLines(txtFileName, new string[] {name, text});
+			
+			await context.Response.WriteAsync("New Entry was added");
+		}
+	}
+}
---
+++ 2 kurs/EmptyWeb/EmptyWeb/Services/EmailMessageSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmptyWeb
+{
+	public class EmailMessageSender : IMessageSender
+	{
+		public string Send()
+		{
+			return "Sent by Email";
+		}
+	}
+
+}
