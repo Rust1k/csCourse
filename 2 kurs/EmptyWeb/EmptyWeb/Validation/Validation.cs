9282271bc5f5bc703d2eb2df9b0384ecb0a70894
Rustem Salimov <rust1k@mail.ru>
2020-02-26 13:14:11 +0300

    [upd] validation emptyweb

---
+++ 2 kurs/EmptyWeb/EmptyWeb/Validation/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace EmptyWeb
+{
+	public class Validation
+	{
+		public ValidationResult Validate(object obj)
+		{
+			// здесь пробежать по всем полям модели с использованием рефлексии
+			// и если они имеют атрибут потомок ValidationAttribute
+			// вызвать соответствующий метод IsValid
+
+			return new ValidationResult(true);
+		}
+	}
+}
---
+++ 2 kurs/EmptyWeb/EmptyWeb/Validation/ValidationResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmptyWeb
+{
+	public class ValidationResult
+	{
+		public bool IsValid { get; }
+		public string ErrMsg { get; }
+
+		public ValidationResult(bool isValid, string errMsg = "")
+		{
+			IsValid = isValid;
+			ErrMsg = errMsg;
+		}
+	};
+
+	}
+}
